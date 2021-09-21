6da48544af956b8bdf7105b44473ba073996f278
Stevemats <stevematindi@gmail.com>
2021-09-21 14:07:01 +0300

    added blake2b and blake2c

--- generate_hash.py
+++ generate_hash.py
@@ -22,9 +22,12 @@ def main():
     print(txt)
     print('\n1. Generate MD5 hash')
     print('2. Generate SHA1 hash')
-    print('3. Generate SHA256 hash')
-    print('4. Generate SHA512 hash')
-    print('5. Exit')
+    print('3. Generate SHA224 hash') 
+    print('4. Generate SHA256 hash') 
+    print('5. Generate SHA512 hash') 
+    print('6. Generate blake2b hash')
+    print ('7. Generate blake2s hash') 
+    print('8. Exit')
     while True:
         try:
             choice = int(input('Enter choice: '))
@@ -35,18 +38,28 @@ def main():
                 sha1_gen()
                 break
             elif choice == 3:
-                sha256_gen()
+                sha224_gen()
                 break
             elif choice == 4:
-                sha512_gen()
+                sha256_gen()
                 break
             elif choice == 5:
+                sha512_gen()
+                break
+            elif choice == 6:
+                blake2b_gen()
+                break
+            elif choice == 7:
+                blake2s_gen()
+                break
+            elif choice == 8:
+                print('\n Program exited!')
                 break
             else:
-                print("Invalid choice. Enter a choice in menu. 1, 2, 3, 4 or 5")
+                print("Invalid choice. Enter a choice in menu. 1 ≦ option ≦ 8")
                 main()
         except ValueError:
-            print("Invalid choice. Enter 1, 2, 3, 4 or 5")
+            print("Invalid choice.")
     exit()
 
 #TO DO: Declare a global var for the message
@@ -67,14 +80,11 @@ def md5_gen():
     message_hash = hashlib.md5(message).hexdigest()
     print("Hash(hexadecimal format)= ", message_hash,"\n")
 
-
 def sha1_gen():
     """
     SHA-1 is a cryptographic hash function that takes
     a message of arbitrary length as input and generates
-    a hash value that is 160-bit in size. Using the hashlib
-    module in Python, we can generate the SHA-1 hash of a message
-    in the following way:
+    a hash value that is 160-bit in size. 
     """
     user_message = input("Enter message to encrypt: ")
     message = user_message.encode('utf-8')
@@ -84,13 +94,24 @@ def sha1_gen():
     print("Hash(hexadecimal format)= ", message_hash,"\n")
 
 
+def sha224_gen():
+    """
+    SHA-224 is intended to provide 112 bits of security, 
+    which is the generally accepted strength of Triple-DES [3DES]. 
+    """
+    user_message = input("Enter message to encrypt: ")
+    message = user_message.encode('utf-8')
+    message_hash = hashlib.sha224(message).digest()
+    print("\nHash(byte format)= ", message_hash,"\n")
+    message_hash = hashlib.sha224(message).hexdigest()
+    print("Hash(hexadecimal format)= ", message_hash,"\n")
+
+
 def sha256_gen():
     """
     SHA-256 is a cryptographic hash function that
     takes a message of any length as input and generates
-    a hash value that is 256-bit in size. Using the hashlib
-    module in Python, we can compute the SHA-256 hash value
-    of a message in the following way:
+    a hash value that is 256-bit in size.
     """
     user_message = input("Enter message to encrypt: ")
     message = user_message.encode('utf-8')
@@ -103,8 +124,6 @@ def sha512_gen():
     """
     SHA-512 takes a message of arbitrary length as
     input and generates a hash value that is 512-bit in size. 
-    We can use the hashlib module in Python to generate the SHA-512
-    hash of a message in the following way:
     """
     user_message = input("Enter message to encrypt: ")
     message = user_message.encode()
@@ -112,6 +131,31 @@ def sha512_gen():
     print("\nHash(byte format)= ",message_hash,"\n")
     message_hash = hashlib.sha512(message).hexdigest()
     print("Hash(hexadecimal format)= ",message_hash,"\n")
+
+def blake2b_gen():
+        """
+        BLAKE2 is a cryptographic hash function faster than 
+        MD5, SHA-1, SHA-2, and SHA-3, yet is at least as secure
+        as the latest standard SHA-3
+        """
+        user_message = input("Enter message to encrypt: ")
+        message = user_message.encode()
+        message_hash = hashlib.blake2b(message).digest()
+        print("\nHash(byte format)= ",message_hash,"\n")
+        message_hash = hashlib.blake2b(message).hexdigest()
+        print("Hash(hexadecimal format)= ",message_hash,"\n")
+
+def blake2s_gen():
+        """
+        BLAKE2s is optimized for 8- to 32-bit platforms and 
+        produces digests of any size between 1 and 32 bytes. 
+        """
+        user_message = input("Enter message to encrypt: ")
+        message = user_message.encode()
+        message_hash = hashlib.blake2s(message).digest()
+        print("\nHash(byte format)= ",message_hash,"\n")
+        message_hash = hashlib.blake2s(message).hexdigest()
+        print("Hash(hexadecimal format)= ",message_hash,"\n")
 
 
 if __name__ == '__main__':
