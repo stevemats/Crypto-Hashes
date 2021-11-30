bdb55a736f5080d4c3e9d4e2626ba619b080e1ba
Stevemats <stevematindi@gmail.com>
2021-11-30 18:28:56 +0300

    minor update

--- generate_hash.py
+++ generate_hash.py
@@ -3,30 +3,18 @@ from __future__ import print_function
 
 import hashlib
 
-print("""
-
-    __  __           __       ______                           __            
-   / / / /___ ______/ /_     / ____/__  ____  ___  _________ _/ /_____  _____
-  / /_/ / __ `/ ___/ __ \   / / __/ _ \/ __ \/ _ \/ ___/ __ `/ __/ __ \/ ___/
- / __  / /_/ (__  ) / / /  / /_/ /  __/ / / /  __/ /  / /_/ / /_/ /_/ / /    
-/_/ /_/\__,_/____/_/ /_/   \____/\___/_/ /_/\___/_/   \__,_/\__/\____/_/     
-                                                                             
- [x] Developed by: Stevematindi
- [x] Main Purpose: Crypto Hashing Use case     
- [x] Usage Demo  : $ python generate_hash.py                                                            
-""")
-
 
 def main():
+    print_banner()
     txt = "Welcome to Hash Generator! Choose one of the hash algorithms below to continue:".title()
     print(txt)
     print('\n1. Generate MD5 hash')
     print('2. Generate SHA1 hash')
-    print('3. Generate SHA224 hash') 
-    print('4. Generate SHA256 hash') 
-    print('5. Generate SHA512 hash') 
+    print('3. Generate SHA224 hash')
+    print('4. Generate SHA256 hash')
+    print('5. Generate SHA512 hash')
     print('6. Generate blake2b hash')
-    print ('7. Generate blake2s hash') 
+    print('7. Generate blake2s hash')
     print('8. Exit')
     while True:
         try:
@@ -62,10 +50,26 @@ def main():
             print("Invalid choice.")
     exit()
 
-#TO DO: Declare a global var for the message
+
+def print_banner():
+
+    print("""
+
+
+
+        __  __           __       ______                           __            
+       / / / /___ ______/ /_     / ____/__  ____  ___  _________ _/ /_____  _____
+      / /_/ / __ `/ ___/ __ \   / / __/ _ \/ __ \/ _ \/ ___/ __ `/ __/ __ \/ ___/
+     / __  / /_/ (__  ) / / /  / /_/ /  __/ / / /  __/ /  / /_/ / /_/ /_/ / /    
+    /_/ /_/\__,_/____/_/ /_/   \____/\___/_/ /_/\___/_/   \__,_/\__/\____/_/     
+                                                                             
+    [x] Developed by: Stevematindi
+    [x] Main Purpose: Crypto Hashing Use case     
+    [x] Usage Demo  : $ python generate_hash.py                                                            
+    """)
+
 
 def md5_gen():
-
     """
     Calculate MD5 hash:
 
@@ -76,9 +80,10 @@ def md5_gen():
     user_message = input("Enter message to encrypt: ")
     message = user_message.encode('utf-8')
     message_hash = hashlib.md5(message).digest()
-    print("\nHash(byte format)= ",message_hash,"\n")
+    print("\nHash(byte format)= ", message_hash, "\n")
     message_hash = hashlib.md5(message).hexdigest()
-    print("Hash(hexadecimal format)= ", message_hash,"\n")
+    print("Hash(hexadecimal format)= ", message_hash, "\n")
+
 
 def sha1_gen():
     """
@@ -89,9 +94,9 @@ def sha1_gen():
     user_message = input("Enter message to encrypt: ")
     message = user_message.encode('utf-8')
     message_hash = hashlib.sha1(message).digest()
-    print("\nHash(byte format)= ", message_hash,"\n")
+    print("\nHash(byte format)= ", message_hash, "\n")
     message_hash = hashlib.sha1(message).hexdigest()
-    print("Hash(hexadecimal format)= ", message_hash,"\n")
+    print("Hash(hexadecimal format)= ", message_hash, "\n")
 
 
 def sha224_gen():
@@ -102,9 +107,9 @@ def sha224_gen():
     user_message = input("Enter message to encrypt: ")
     message = user_message.encode('utf-8')
     message_hash = hashlib.sha224(message).digest()
-    print("\nHash(byte format)= ", message_hash,"\n")
+    print("\nHash(byte format)= ", message_hash, "\n")
     message_hash = hashlib.sha224(message).hexdigest()
-    print("Hash(hexadecimal format)= ", message_hash,"\n")
+    print("Hash(hexadecimal format)= ", message_hash, "\n")
 
 
 def sha256_gen():
@@ -116,9 +121,10 @@ def sha256_gen():
     user_message = input("Enter message to encrypt: ")
     message = user_message.encode('utf-8')
     message_hash = hashlib.sha256(message).digest()
-    print("\nHash(byte format)= ", message_hash,"\n","\n")
+    print("\nHash(byte format)= ", message_hash, "\n", "\n")
     message_hash = hashlib.sha256(message).hexdigest()
-    print("Hash(hexadecimal format)= ", message_hash,"\n")
+    print("Hash(hexadecimal format)= ", message_hash, "\n")
+
 
 def sha512_gen():
     """
@@ -128,34 +134,36 @@ def sha512_gen():
     user_message = input("Enter message to encrypt: ")
     message = user_message.encode()
     message_hash = hashlib.sha512(message).digest()
-    print("\nHash(byte format)= ",message_hash,"\n")
+    print("\nHash(byte format)= ", message_hash, "\n")
     message_hash = hashlib.sha512(message).hexdigest()
-    print("Hash(hexadecimal format)= ",message_hash,"\n")
+    print("Hash(hexadecimal format)= ", message_hash, "\n")
+
 
 def blake2b_gen():
-        """
-        BLAKE2 is a cryptographic hash function faster than 
-        MD5, SHA-1, SHA-2, and SHA-3, yet is at least as secure
-        as the latest standard SHA-3
-        """
-        user_message = input("Enter message to encrypt: ")
-        message = user_message.encode()
-        message_hash = hashlib.blake2b(message).digest()
-        print("\nHash(byte format)= ",message_hash,"\n")
-        message_hash = hashlib.blake2b(message).hexdigest()
-        print("Hash(hexadecimal format)= ",message_hash,"\n")
+    """
+    BLAKE2 is a cryptographic hash function faster than 
+    MD5, SHA-1, SHA-2, and SHA-3, yet is at least as secure
+    as the latest standard SHA-3
+    """
+    user_message = input("Enter message to encrypt: ")
+    message = user_message.encode()
+    message_hash = hashlib.blake2b(message).digest()
+    print("\nHash(byte format)= ", message_hash, "\n")
+    message_hash = hashlib.blake2b(message).hexdigest()
+    print("Hash(hexadecimal format)= ", message_hash, "\n")
+
 
 def blake2s_gen():
-        """
-        BLAKE2s is optimized for 8- to 32-bit platforms and 
-        produces digests of any size between 1 and 32 bytes. 
-        """
-        user_message = input("Enter message to encrypt: ")
-        message = user_message.encode()
-        message_hash = hashlib.blake2s(message).digest()
-        print("\nHash(byte format)= ",message_hash,"\n")
-        message_hash = hashlib.blake2s(message).hexdigest()
-        print("Hash(hexadecimal format)= ",message_hash,"\n")
+    """
+    BLAKE2s is optimized for 8- to 32-bit platforms and 
+    produces digests of any size between 1 and 32 bytes. 
+    """
+    user_message = input("Enter message to encrypt: ")
+    message = user_message.encode()
+    message_hash = hashlib.blake2s(message).digest()
+    print("\nHash(byte format)= ", message_hash, "\n")
+    message_hash = hashlib.blake2s(message).hexdigest()
+    print("Hash(hexadecimal format)= ", message_hash, "\n")
 
 
 if __name__ == '__main__':
